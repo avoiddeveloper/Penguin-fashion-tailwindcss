1132b45964dadbc24c07af9194731a274d5fbcb3
avoiddeveloper <karimwdd03@gmail.com>
2024-10-25 03:53:21 +0600

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'bebas': ['Bebas Neue', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
